ff3267a196532bee2c64cdb65bafbb617e785889
salzamt <salzamt@live.com>
2017-07-31 17:03:53 +0200

    Update startup.sh

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -       
-#description  :Provision script for starting the morecast-api on vagrant, starts all needed services
 #author       :rsalzer
 #date         :20151015
 #=======================================================================================================================
@@ -7,9 +6,3 @@
 ########################################################################################################################
 ################################################# lets get se party started ############################################
 ########################################################################################################################
-killall python
-killall uwsgi
-service nginx restart
-/bin/bash /usr/share/vagrant/startup/start_morecast_api.sh &
-/bin/bash /usr/share/vagrant/startup/start_dataserver.sh &
-/bin/bash /usr/share/vagrant/startup/start_timezone_api.sh &
